5661157aead5c5b197291419266baf7bbb1ce17b
Andrew Fahlgren <andrewfahlgren@gmail.com>
2023-01-10 10:13:34 -0800

    Added code to grey out marks which are not a part of the selected recipe mark's reconstructed recipe

--- Scripts/Services/RecipeBookUIService.cs
+++ Scripts/Services/RecipeBookUIService.cs
@@ -4,6 +4,10 @@ using PotionCraft.ObjectBased.UIElements;
 using PotionCraft.ObjectBased.UIElements.Books;
 using PotionCraft.ObjectBased.UIElements.Books.RecipeBook;
 using PotionCraftUsefulRecipeMarks.Scripts.Storage;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
 
 namespace PotionCraftUsefulRecipeMarks.Scripts.Services
 {
@@ -21,10 +25,27 @@ namespace PotionCraftUsefulRecipeMarks.Scripts.Services
 
             StaticStorage.SelectedRecipeMarkIndex = markIndex;
 
+            //Update page with reconstructed recipe
             var leftPage = Managers.Potion.recipeBook.curlPageController.frontLeftPage;
             var rightPage = Managers.Potion.recipeBook.curlPageController.frontRightPage;
             leftPage.UpdatePageContent(PageContent.State.Filled, recipe);
             rightPage.UpdatePageContent(PageContent.State.Filled, recipe);
+
+            //Update recipe marks to show selection
+            var rightPageContent = (RecipeBookRightPageContent)rightPage.pageContent;
+            var visibleMarksDict = (Dictionary<int, List<RecipeBookRecipeMark>>)Traverse.Create(rightPageContent).Field("visibleMarks").GetValue();
+            var allMarks = visibleMarksDict.Values.SelectMany(v => v).ToList();
+            var markDisabledColor = new Color(1, 1, 1, 0.5f);
+            var markEnabledColor = new Color(1, 1, 1, 1);
+            allMarks.ForEach(mark =>
+            {
+                var isDisabled = mark.currentMarkIndex > markIndex;
+                var iconSpriteRenderer = mark.GetComponentInChildren<SpriteRenderer>();
+                iconSpriteRenderer.color = isDisabled ? markDisabledColor : markEnabledColor;
+                var text = mark.GetComponentInChildren<TextMeshPro>();
+                text.color = isDisabled ? markDisabledColor : markEnabledColor;
+
+            });
         }
     }
 }
